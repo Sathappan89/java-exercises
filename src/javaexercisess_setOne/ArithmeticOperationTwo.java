78792e09bdbe3b4b1adb79a5810311517aa49e95
Sathappan89 <sathappanas1989@gmail.com>
2025-11-08 13:42:30 -0700

    ArithmeticOperation

---
+++ src/javaexercisess_setOne/ArithmeticOperationTwo.java
@@ -0,0 +1,12 @@
+package javaexercisess_setOne;
+
+public class ArithmeticOperationTwo {
+
+	public static void main(String[] args) {
+
+		// (7/2)*2.0
+		System.out.println((7 / 2) * 2.0);
+
+	}
+
+}
