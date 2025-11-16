f136cdcf8eaebe5ebc0642cfae45a388368f9b5c
Sathappan89 <sathappanas1989@gmail.com>
2025-11-15 23:21:30 -0700

    Java_NestedLoop

---
+++ src/javaexercise_setTwo/Question3.java
@@ -0,0 +1,24 @@
+package javaexercise_setTwo;
+
+/*
+ * 1
+ * 2 1
+ * 3 2 1
+ * 4 3 2 1
+ * */
+public class Question3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = i; j >= 1; j--) {
+				if (i >= j) {
+					System.out.print(j + " ");
+				}
+
+			}
+			System.out.println(" ");
+
+		}
+
+	}
+}
