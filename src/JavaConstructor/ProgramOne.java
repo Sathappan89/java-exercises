98e210e236df4a0064b32cdd87e3b31d3f32b50e
Sathappan89 <sathappanas1989@gmail.com>
2025-12-18 21:57:15 -0700

    DefaultConstructorInJava

---
+++ src/JavaConstructor/ProgramOne.java
@@ -0,0 +1,26 @@
+package JavaConstructor;
+
+public class ProgramOne {
+	/*
+	 * Write a Java program to create a class called "Cat" with instance variables
+	 * name and age. Implement a default constructor that initializes the name to
+	 * "Unknown" and the age to 0. Print the values of the variables.
+	 */
+
+	public String catName;
+	public int catAge;
+
+	ProgramOne() {
+		System.out.println("Default Constructor");
+	}
+
+	public static void main(String[] args) {
+
+		ProgramOne c = new ProgramOne();
+		c.catAge = 10;
+		c.catName = "sathappan";
+		System.out.println(c.catAge + " " + c.catName);
+
+	}
+
+}
