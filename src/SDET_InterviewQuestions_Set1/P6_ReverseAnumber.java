ee770caf4ab5d11c158c2b452ec7b10cc2e5d67f
Sathappan89 <sathappanas1989@gmail.com>
2025-12-25 07:52:12 -0700

    Reverse A Number

---
+++ src/SDET_InterviewQuestions_Set1/P6_ReverseAnumber.java
@@ -0,0 +1,35 @@
+package SDET_InterviewQuestions_Set1;
+
+import java.util.Scanner;
+
+public class P6_ReverseAnumber {
+	/* Java Program to reverse number */
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number ");
+		int number = scanner.nextInt();
+
+//		int rev = number % 10;
+//		System.out.println(rev); // Remainder
+//
+//		int q = number / 10;
+//		System.out.println(q); // Quotient
+		
+		
+		int reverseNumber = 0;
+		
+		while(number>0) {
+			
+			reverseNumber = number%10;
+			
+			System.out.print(reverseNumber + "");
+			number=number/10;
+			
+			
+			
+		}
+
+	}
+
+}
