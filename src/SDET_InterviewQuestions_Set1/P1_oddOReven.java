4e9f18cddfc58411c1db16c4e92b6f76e6fc3b89
Sathappan89 <sathappanas1989@gmail.com>
2025-12-24 19:28:36 -0700

    JP Odd or Even

---
+++ src/SDET_InterviewQuestions_Set1/P1_oddOReven.java
@@ -0,0 +1,22 @@
+package SDET_InterviewQuestions_Set1;
+/*Java Program to find odd or even number*/
+import java.util.Scanner;
+
+public class P1_oddOReven {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+
+		int number = scanner.nextInt();
+
+		if (number % 2 == 0) {
+			System.out.println("Number is the Even Number " + number);
+		} else {
+			System.out.println("Number is the Even Number " + number);
+		}
+
+	}
+
+}
