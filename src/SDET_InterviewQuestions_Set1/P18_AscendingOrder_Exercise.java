9ceb27e41a2e30a3889c8caa747ad5c32a8a322b
Sathappan89 <sathappanas1989@gmail.com>
2025-12-28 20:33:00 -0700

    TenPrimeNumber

---
+++ src/SDET_InterviewQuestions_Set1/P18_AscendingOrder_Exercise.java
@@ -0,0 +1,10 @@
+package SDET_InterviewQuestions_Set1;
+
+public class P18_AscendingOrder_Exercise {
+/*Write the Java Program to arrange the number in the ascending order*/
+	public static void main(String[] args) {
+	
+		
+
+	}
+}
---
+++ src/SDET_InterviewQuestions_Set1/primeNumberprint.java
@@ -0,0 +1,31 @@
+package SDET_InterviewQuestions_Set1;
+
+public class primeNumberprint {
+
+	public static void main(String[] args) {
+
+		int num = 2;
+		int count = 0;
+
+		while (count < 10) {
+
+			boolean isPrime = true;
+
+			for (int i = 2; i < num; i++) {
+				if (num % i == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime) {
+				System.out.println(num);
+				count++;
+			}
+
+			num++;
+		}
+
+	}
+
+}
