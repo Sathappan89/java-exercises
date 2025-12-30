194397cf0fbf185fdbec26470e666c502f3011b4
Sathappan89 <sathappanas1989@gmail.com>
2025-12-29 20:27:02 -0700

    SDET_Array_Question_1

---
+++ src/SDET_InterviewQuestions_Set2/P1_LargestandSmallest.java
@@ -0,0 +1,41 @@
+package SDET_InterviewQuestions_Set2;
+
+import java.util.Scanner;
+
+public class P1_LargestandSmallest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter " + size + " numbers:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();  // reading each number
+        }
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Largest number: " + largest);
+        System.out.println("Smallest number: " + smallest);
+
+        sc.close();
+    }
+}
