790aa13c1a8d5ee66524308cf4a543cb1d5b6b78
Sathappan89 <sathappanas1989@gmail.com>
2025-12-30 21:40:21 -0700

    AscendingOrder_BubbleSort

--- src/SDET_InterviewQuestions_Set2/P6_AscendingOrder.java
+++ src/SDET_InterviewQuestions_Set2/P6_AscendingOrder.java
@@ -4,33 +4,30 @@ public class P6_AscendingOrder {
 
 	public static void main(String[] args) {
 
-		int arr[] = { 5, 7, 1, 3, 6, 8, 2, 9, 4 };
+		int arr[] = { 7,6,4,3};
 		int len = arr.length;
-		System.out.println("Enter the length of the array : " + len);
+		System.out.println("Enter the length of the array : " + len); //4
 
 		System.out.println("Original Array:");
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 
-		// Bubble Sort using index-based loops
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 0; j < arr.length - 1 - i; j++) {
-
-				if (arr[j] > arr[j + 1]) {
-					// swap arr[j] and arr[j+1]
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
+		for(int j=len-1;j>=1;j--) {
+			for(int i=0;i<j;i++) {
+				if(arr[i]>arr[i+1]) {
+					int temp = arr[i];
+					arr[i]=arr[i+1];
+					arr[i+1]=temp;
 				}
 			}
 		}
-
-		System.out.println("\n\nSorted Array (Ascending):");
+		System.out.println(" ");
+		System.out.println("Ascending Array:" + " ");
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
-
+		
 	}
 
 }
