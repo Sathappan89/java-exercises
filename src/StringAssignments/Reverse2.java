8b7b13020fbd4924e9536a050f7c6574973259b4
Sathappan89 <sathappanas1989@gmail.com>
2026-03-22 19:41:51 -0700

    String Reverse

---
+++ src/StringAssignments/Reverse2.java
@@ -0,0 +1,14 @@
+package StringAssignments;
+
+public class Reverse2 {
+
+	public static void main(String[] args) {
+		StringBuilder b1 = new StringBuilder("Selenium");
+		System.out.println(b1.reverse());
+
+		StringBuffer b2 = new StringBuffer("HelloTesting");
+		System.out.println(b2.reverse());
+
+	}
+
+}
