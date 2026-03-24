d73d8b5199e3fc32658778090a521a812d82a420
Sathappan89 <sathappanas1989@gmail.com>
2026-03-24 13:27:46 -0700

    Initializer.Java

---
+++ src/JavaStaticMembers/Initializer.java
@@ -0,0 +1,21 @@
+package JavaStaticMembers;
+
+/*Write a Java program to create a class called "Initializer" with a static block that initializes a static variable 'initialValue' to 1000. 
+ * Print the value of 'initialValue' before and after creating an instance of "Initializer".*/
+public class Initializer {
+
+	// Declare a static variable initialValue
+	static int initialValue;
+
+	static {
+		initialValue = 1000;
+		System.out.println("Static block: initialValue initialized to " + initialValue);
+	}
+
+	public static void main(String[] args) {
+		Initializer in = new Initializer();
+		System.out.println("After creating an instance : initialValue = " + Initializer.initialValue);
+
+	}
+
+}
