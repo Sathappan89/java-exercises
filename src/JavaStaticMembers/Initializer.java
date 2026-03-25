d1c717f63f258de676f8044a709d19fc4110b535
Sathappan89 <sathappanas1989@gmail.com>
2026-03-25 10:01:32 -0700

    Initializer

--- src/JavaStaticMembers/Initializer.java
+++ src/JavaStaticMembers/Initializer.java
@@ -7,6 +7,7 @@ public class Initializer {
 	// Declare a static variable initialValue
 	static int initialValue;
 
+	// Static block to initialize initialValue
 	static {
 		initialValue = 1000;
 		System.out.println("Static block: initialValue initialized to " + initialValue);
