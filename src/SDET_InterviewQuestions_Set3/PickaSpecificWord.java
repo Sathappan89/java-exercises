2f261d42ceb5b52fa991a239190db02ab5b3feeb
Sathappan89 <sathappanas1989@gmail.com>
2026-04-05 18:18:04 -0700

    Pick a Specific Word from Sentence

---
+++ src/SDET_InterviewQuestions_Set3/PickaSpecificWord.java
@@ -0,0 +1,30 @@
+package SDET_InterviewQuestions_Set3;
+
+import java.util.HashMap;
+
+public class PickaSpecificWord {
+
+	public static HashMap<Integer, String> wordPick(String str) {
+		String[] words = str.split(" ");
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+		for (int i = 0; i < words.length; i++) {
+			map.put(i, words[i]);
+		}
+
+		for (String x : map.values()) {
+			if (x.equals("learning")) {
+				System.out.println("learning");
+			}
+		}
+
+		return map;
+
+	}
+
+	public static void main(String[] args) {
+		String str = "I love learning Java Exercises";
+		PickaSpecificWord.wordPick(str);
+	}
+
+}
