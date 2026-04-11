9ab68a6df5b616e215a1dcae3e693c5733b1da92
Sathappan89 <sathappanas1989@gmail.com>
2026-04-10 18:09:20 -0700

    Count Vowels and Consonants

---
+++ src/SDET_InterviewQuestions_Set3/ExerciseSix.java
@@ -0,0 +1,28 @@
+package SDET_InterviewQuestions_Set3;
+
+public class ExerciseSix {
+	/* Count vowels and consonants */
+	/* q e i o u */
+	public static void main(String[] args) {
+		String str = "I live to learn Java Strings";
+
+        int countVowels = 0;
+        int countConsonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+
+            if (ch >= 'a' && ch <= 'z') {        // Only letters
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    countVowels++;
+                } else {
+                    countConsonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels count      : " + countVowels);
+        System.out.println("Consonants count  : " + countConsonants);
+	}
+
+}
