6a9bf7dbbf4cf859ee9771f7210f72fab0894857
Sathappan89 <sathappanas1989@gmail.com>
2026-04-10 18:43:33 -0700

    Remove duplicate characters

--- src/SDET_InterviewQuestions_Set3/ExerciseNine.java
+++ src/SDET_InterviewQuestions_Set3/ExerciseNine.java
@@ -1,10 +1,29 @@
 package SDET_InterviewQuestions_Set3;
 
+import java.util.HashSet;
+
 public class ExerciseNine {
+//Remove duplicate characters
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "java leason";
 
+		int n = str.length();
+
+		StringBuilder sb = new StringBuilder();
+
+		HashSet<Character> myset = new HashSet<Character>();
+
+		for (int i = 0; i < n; i++) {
+			char ch = str.charAt(i);
+			if (myset.contains(ch)) {
+				continue;
+			} else {
+				myset.add(ch);
+				sb.append(ch);
+			}
+		}
+		System.out.println(sb.toString());
 	}
 
 }
